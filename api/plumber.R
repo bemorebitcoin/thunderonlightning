dcf398c3bd46c11f6084f139624c2f8e9c90e4c4
bemorebitcoin <38768431+bemorebitcoin@users.noreply.github.com>
2026-01-29 15:56:50 +0000

    Update plumber.R

--- api/plumber.R
+++ api/plumber.R
@@ -51,7 +51,7 @@ function(id, amount, comment = "") {
   )
   
   list(
-    pr = "lnbc1REPLACE_WITH_REAL_INVOICE",
+    pr = "lnbc1p5hhq59pp5xrkrx8vsmpfdhq7gn235p3gglaxnr9umw2zw0rxf2ykw2fk27lgsdqqcqzzsxqrrs0fppqzkk5sjlvqa9f767kkandvjz07dzayju4sp5kly6waup2fk0askspyfmlp9hdh264gewrac8cyplwrw667zuf4kq9qxpqysgq7ja5rc7h23qw9vkl9nhxw79x08m8vpuhfr62frkpez3q6ca44lfkrp5x4tgh3nau9w6utyld7yanmruqmcwkl7a3qkjc8vy7zajuk2splulwy5",
     routes = list()
   )
 }
